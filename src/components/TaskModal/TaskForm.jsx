ba0468317064e856d140b776d67bee01f6f8631f
Maksym-Plutenko <plutenkom@gmail.com>
2023-06-27 16:07:44 +0300

    radio styles

--- src/components/TaskModal/TaskForm.jsx
+++ src/components/TaskModal/TaskForm.jsx
@@ -7,6 +7,9 @@ import { styled } from 'styled-components';
 
 // import { validate } from './utility/validateTaskForm';
 
+import {taskColors} from '../../styles/variables';
+
+
 // import { Button } from '../../styles/components';
 
 const Label = styled.label`
@@ -58,18 +61,57 @@ const RadioContainer = styled.div`
   display: flex;
   gap: 16px;
   width: 340px;
+  padding-left: 3px;
 
   margin-bottom: 32px;
+
+  // & > label[]
+`;
+
+const RadioButton = styled.input`
+  visibility: hidden;
+  opacity: 0;
+  position: absolute;
+
+  &:checked + label::before {
+    outline: 3px solid ${taskColors.priorityLow};
+  }
+
+  &:checked + label[for="medium"]::before {
+    outline-color: ${taskColors.priorityMedium};
+  }
+
+  &:checked + label[for="high"]::before {
+    outline-color: ${taskColors.priorityHigh};
+  }
 `;
 
 const RadioLabel = styled.label`
-font-family: Inter;
-font-size: 14px;
-font-weight: 600;
-line-height: 18px;
-letter-spacing: 0em;
-color: #343434;
+  font-family: Inter;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 18px;
+  letter-spacing: 0em;
+  color: #343434;
 
+  &::before {
+    content: '';
+    display: inline-block;
+    width: 11px;
+    height: 11px;
+    border-radius: 50%;
+    background-color: ${taskColors.priorityLow};
+    border: 1px solid white;
+    margin-right: 5px;
+  }
+
+  &[for="medium"]::before {
+    background-color: ${taskColors.priorityMedium};
+  }
+
+  &[for="high"]::before {
+    background-color: ${taskColors.priorityHigh};
+  }
 `;
 
 const ButtonContainer = styled.div`
@@ -81,12 +123,18 @@ const ButtonContainer = styled.div`
 const Button = styled.button`
   width: 100%;
   padding: 14px;
-  color: white;
   background-color: #3e85f3;
   border-radius: 8px;
-
   border: none;
   outline: none;
+
+  font-family: Inter;
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 18px;
+  letter-spacing: 0em;
+  text-align: center;
+  color: white;
 `;
 
 const TaskForm = ({ close, create, task }) => {
@@ -177,36 +225,33 @@ const TaskForm = ({ close, create, task }) => {
         </TimeContainer>
 
         <RadioContainer>
-          <RadioLabel>
-            Low
-            <input
-              name="priority"
-              type="radio"
-              onChange={formik.handleChange}
-              value="low"
-              checked={formik.values.priority === 'low'}
-            />
-          </RadioLabel>
-          <RadioLabel>
-            Medium
-            <input
-              name="priority"
-              type="radio"
-              onChange={formik.handleChange}
-              value="medium"
-              checked={formik.values.priority === 'medium'}
-            />
-          </RadioLabel>
-          <RadioLabel>
-            High
-            <input
-              name="priority"
-              type="radio"
-              onChange={formik.handleChange}
-              value="high"
-              checked={formik.values.priority === 'high'}
-            />
-          </RadioLabel>
+          <RadioButton
+            id="low"
+            name="priority"
+            type="radio"
+            onChange={formik.handleChange}
+            value="low"
+            checked={formik.values.priority === 'low'}
+          />
+          <RadioLabel for="low">Low</RadioLabel>
+          <RadioButton
+            id="medium"
+            name="priority"
+            type="radio"
+            onChange={formik.handleChange}
+            value="medium"
+            checked={formik.values.priority === 'medium'}
+          />
+          <RadioLabel for="medium">Medium</RadioLabel>
+          <RadioButton
+            id="high"
+            name="priority"
+            type="radio"
+            onChange={formik.handleChange}
+            value="high"
+            checked={formik.values.priority === 'high'}
+          />
+          <RadioLabel for="high">High</RadioLabel>
         </RadioContainer>
         {/* <p>{formik.values.priority}</p> */}
       </div>
